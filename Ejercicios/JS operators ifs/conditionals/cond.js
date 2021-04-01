a56eaae4edd7466e32c0153ed8f11bfced99a013
PegaChucho <jesus_perez_0123@hotmail.com>
2021-04-01 09:36:50 -0600

    checkpoint

---
+++ Ejercicios/JS operators ifs/conditionals/cond.js
@@ -0,0 +1,25 @@
+// console.log("before conditional")
+// let random = Math.random();
+
+// if (random < 0.5) {
+//     console.log("less than 0.5")
+//     console.log(random)
+// } else if (random > 0.5) {
+//     console.log("bigger than 0.5")
+//     console.log(random)
+// }
+
+
+// console.log("after")
+
+const age = 25;
+
+if (age < 5) {
+    console.log("You're a child, you get in for free")
+} else if (age < 10) {
+    console.log("You're a kid, you get in for $10")
+} else if (age < 65) {
+    console.log("You're an adult, you get in for $20")
+} else {
+    console.log("You're an senior, you get in for $10")
+}
---
+++ Ejercicios/JS operators ifs/first script/app.js
@@ -0,0 +1,4 @@
+console.log("hello from my first JS file");
+let total = 1 + 3
+console.log(total)
+console.log("goodbye");
